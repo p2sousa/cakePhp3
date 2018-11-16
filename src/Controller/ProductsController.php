8de391b6db34a4e7829bf755453913d1c8dabfe0
Pablo Sousa <pablosousa.ads@gmail.com>
2018-11-15 23:54:04 -0200

    Controller: load Services in methods add and edit.

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use App\Service\Form\ProductFormService;
+use App\Service\Manager\ProductManagerService;
 
 /**
  * Products Controller
@@ -35,7 +37,7 @@ class ProductsController extends AppController
     public function view(string $id = null): void
     {
         $product = $this->Products->get($id, [
-            'contain' => []
+            'contain' => ['Tags']
         ]);
 
         $this->set('product', $product);
@@ -48,41 +50,54 @@ class ProductsController extends AppController
      */
     public function add()
     {
-        $product = $this->Products->newEntity();
-        if ($this->request->is('post')) {
-            $product = $this->Products->patchEntity($product, $this->request->getData());
-            if ($this->Products->save($product)) {
+        try {
+            if ($this->request->is('post')) {
+                
+                $productManager = new ProductManagerService($this);
+                $productManager->create();
+                
                 $this->Flash->success(__('The product has been saved.'));
-
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The product could not be saved. Please, try again.'));
+            
+        } catch (\Exception $exc) {
+            $this->Flash->error(
+                __($exc->getMessage())
+            );
         }
-        $this->set(compact('product'));
+        
+        $productForm = new ProductFormService($this);
+        $productForm->newForm();
     }
 
     /**
      * Edit method
      *
-     * @param string|null $id Product id.
+     * @param int|null $id Product id.
      * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
      * @throws \Cake\Network\Exception\NotFoundException When record not found.
      */
-    public function edit(string $id = null)
+    public function edit(int $id = null)
     {
-        $product = $this->Products->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $product = $this->Products->patchEntity($product, $this->request->getData());
-            if ($this->Products->save($product)) {
+        try {
+            if ($this->request->is(['patch', 'post', 'put'])) {
+                
+                $productManager = new ProductManagerService($this);
+                $productManager->setProduct($id);
+                $productManager->update();
+                
                 $this->Flash->success(__('The product has been saved.'));
-
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The product could not be saved. Please, try again.'));
+        } catch (\Exception $exc) {
+            $this->Flash->error(
+                __($exc->getMessage())
+            );
         }
-        $this->set(compact('product'));
+        
+        $productForm = new ProductFormService($this);
+        $productForm->setProduct($id);
+        $productForm->editForm();
     }
 
     /**
